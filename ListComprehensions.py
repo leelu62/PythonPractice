2d43e9091112f13fea3e2a8dcb1d6f7cf79043b0
Jenna Shannon <jenna.r.shannon@gmail.com>
2018-11-01 08:03:17 -0400

    Adding file

---
+++ ListComprehensions.py
@@ -0,0 +1,10 @@
+# for a given list, write one line of code that creates a new list containing only the even elements of
+#the first list
+a=[1,4,9,16,25,36,49,64,81,100]
+even = [i for i in a if i%2==0]
+print(even)
+
+
+
+
+
